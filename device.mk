6cddfb2754ba424121ecc4ff81dccd98cc297ec8
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-24 17:51:31 -0400

    phoenix: Add device specific init script
    Change-Id: I4e50da5f92ed485d33f492b357e4dab993703491

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.phoenix.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.phoenix.rc
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
