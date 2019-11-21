5eb5a80b2a40e658af1828aba670ccabe0b59f41
LuK1337 <priv.luk@gmail.com>
2019-11-21 20:41:32 +0100

    phoenix: Kang AVB setup from coral
    Change-Id: I8349a3eaf092c60485225aeedb949f52f8eeb071

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,5 +33,12 @@ TARGET_KERNEL_CONFIG := vendor/lineage_phoenix_defconfig
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
 
+# Verified Boot
+BOARD_AVB_VBMETA_SYSTEM := system
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/phoenix/BoardConfigVendor.mk
