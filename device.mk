f5aad5624d1dfaf4f7bdcd5d4a90263c39db49c2
merothh <merothh@gmail.com>
2020-06-23 13:18:36 +0530

    phoenix: import keylayouts/idc configs
    * Disable DPAD_HOME
    
    Co-authored-by: Akhil Narang <me@akhilnarang.dev>
    Change-Id: I966feeb406dafe559f4f0c6de7d9916ed7ed96b8

--- device.mk
+++ device.mk
@@ -41,6 +41,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
